4c99643da5c5d5def3c5fc292f1c969f08b85eb0
Hugolo6 <pacom6985@gmail.com>
2026-02-18 15:30:07 -0600

    contesta lalo

--- Proyecto_hugo/prueba_arbol.py
+++ Proyecto_hugo/prueba_arbol.py
@@ -8,3 +8,4 @@ arb.construir_desde_infija(expr)
 print("Preorden:", arb.recorrido_preorden())
 print("Inorden:", arb.recorrido_inorden())
 print("Postorden:", arb.recorrido_postorden())
+#lalo te toca hacer todo el proyeto
